97c9f307b22ae916d91aad95182fe59c2be8b39c
Smaragdus7 <smar.leeft@gmail.com>
2023-04-03 16:35:25 -0500

    Hooks and state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(note){
+    setNotes((prevNotes) => {
+      return [...prevNotes, note];
+    });
+  }
+
+  function deleteNote(id){
+    setNotes((prevNotes) => {
+      return prevNotes.filter((note, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
-      {notes.map((note) => (
-          <Note
-            key={note.id}
-            title={note.title}
-            content={note.content}
-          />
-        ))}
+      <CreateArea onAdd={addNote}/>
+      {notes.map((note, index) => (
+        <Note onDelete={deleteNote} key={index} id={index} title={note.title} content={note.content} />
+      ))}
       <Footer />
     </div>
   );
 }
 
 export default App;
+
+//- Pass the new note back to the App.
+//- Add new note to an array.
+//- Take array and render seperate Note components for each item.
+
+//2. Implement the delete note functionality.
+//- Callback from the Note component to trigger a delete function.
+//- Use the filter function to filter out the item that needs deletion.
+//- Pass a id over to the Note component, pass it back to the App when deleting.
